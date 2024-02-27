40ce23253e21e6b20b1893ada0f53585cb9c3b15
Walter Kiprono <walterkiprono81@gmail.com>
2024-02-27 11:44:01 +0300

    feat: add app config

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+function load() {
+    let object = [];
+    var xhr = new XMLHttpRequest();
+
+    // open a connection
+    xhr.open("GET", "assets/json/wonders.json", true);
+    
+    // send the request
+    xhr.send();
+    
+    // handle the response
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // parse the data from json
+        object = JSON.parse(this.responseText);
+        let menus = document.getElementById("header-menus");
+
+        // check if object is an instance of Array before iterating
+        if(object instanceof Array) {
+            // iterate through the items
+            object.forEach((value) => {
+                // Create a li element
+                menus.innerHTML += `<li>${value.label}</li>`;
+            });
+        }
+      }
+    }
+}
+
+load();
