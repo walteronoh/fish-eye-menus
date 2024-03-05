44814f637bda1af50c7584ab7f06ffbcb95ab52e
Walter Kiprono <walterkiprono81@gmail.com>
2024-03-05 14:23:34 +0300

    feat: add active banner

---
+++ README.md
@@ -0,0 +1,16 @@
+<div align="center"><img src="assets/images/banner.png" alt="Logo" width="200"/></div>
+<h1 align="center">FISH EYE MENUS</h1>
+
+## Description
+This project shows the application of Fish Eye menu. All the menu items are displayed on the user interface at once. When a menu item is pointed or clicked at, it becomes “larger” compared with the rest of the menu items.
+
+## Running
+Install the packages:
+```bash
+npm install
+```
+
+Start the application:
+```bash
+npm start
+```
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,31 +4,18 @@ async function loadData() {
 }
 
 async function load() {
-    let object = [];
-    var xhr = new XMLHttpRequest();
+    const object = await loadData();
+    // check if object is an instance of Array
+    if (object instanceof Array) {
+        // parse the data from json
+        let menus = document.getElementById("header-menus");
 
-    // open a connection
-    xhr.open("GET", "assets/json/wonders.json", true);
 
-    // send the request
-    xhr.send();
-
-    // handle the response
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            // parse the data from json
-            object = JSON.parse(this.responseText);
-            let menus = document.getElementById("header-menus");
-
-            // check if object is an instance of Array before iterating
-            if (object instanceof Array) {
-                // iterate through the items
-                object.forEach((value) => {
-                    // Create li element
-                    menus.innerHTML += `<li class="header-element" onclick="handleClick('${value.name}')"><div class="header-image"><span class="header-label">${value.label}</span><img src="assets/images/${value.image}" /></div></li>`;
-                });
-            }
-        }
+        // iterate through the items
+        object.forEach((value) => {
+            // Create li element
+            menus.innerHTML += `<li class="header-element" id="${value.name}" onclick="handleClick('${value.name}')"><div class="header-image"><span class="header-label">${value.label}</span><img src="assets/images/${value.image}" /></div></li>`;
+        });
     }
 }
 
@@ -36,12 +23,24 @@ load();
 
 // set menu as active
 async function handleClick(name) {
+    // Remove the current active menu
+    let elements = document.getElementsByClassName("active");
+    if (elements.length > 0) {
+        elements[0].classList.remove("active");
+    }
+    var div = document.querySelector(`#${name} > div`);
+    var span = document.createElement("span");
+    // Set active menu
+    span.classList.add("active");
+    div.appendChild(span);
+
     const data = await loadData();
     if (data instanceof Array) {
         let currentWonder = data.find((value) => value.name == name);
         let descriptionElement = document.getElementById("description");
         descriptionElement.innerHTML = `<div>
-        <h3>${currentWonder.label}</h3>
+        <h2>${currentWonder.label}</h2>
+        <img src="assets/images/${currentWonder.image}" />
         <p>${currentWonder.description}</p>
         <a href="${currentWonder.link}">Read More</a>
         </div>`;
